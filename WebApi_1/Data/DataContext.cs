03eb373c580dc6b9a949f1b3a8b1d32b93a9ff2c
unknown <dpardrey@gmail.com>
2020-04-30 12:29:50 -0400

    Initial Commit

---
+++ WebApi_1/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApi_1.Models;
+
+namespace WebApi_1.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
